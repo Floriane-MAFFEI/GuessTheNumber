1f45dcf9164262b6bed29e4f0e217c8c6378b187
Floriane-MAFFEI <floriane.oclock@gmail.com>
2023-10-23 11:54:01 +0200

    Random Number

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 import { prompt } from "./prompt.js";
+
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+console.log(getRandomNumber(0, 100));
